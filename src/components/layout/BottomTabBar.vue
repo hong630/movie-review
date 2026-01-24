738ea370fe2b893d70abb4545915c50e02618ed5
hong630 <hongihongi60@gmail.com>
2026-01-25 01:38:59 +0900

    탐색 페이지 스타일

--- src/components/layout/BottomTabBar.vue
+++ src/components/layout/BottomTabBar.vue
@@ -1,18 +1,22 @@
 <template>
   <nav class="tabbar">
     <RouterLink to="/search" class="tab" active-class="active" exact-active-class="active">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M512 128C514 128 515.9 128.1 517.8 128.3L422.1 224L490 224L562 152C570.8 163 576 176.9 576 192L576 448C576 483.3 547.3 512 512 512L128 512C92.7 512 64 483.3 64 448L64 192C64 156.7 92.7 128 128 128L198.1 128L102.1 224L170 224L265 129L266 128L358.1 128L262.1 224L330 224L425 129L426 128L512.1 128z"/></svg>
       탐색
     </RouterLink>
 
     <RouterLink to="/watchlist" class="tab" active-class="active" exact-active-class="active">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M192 64C156.7 64 128 92.7 128 128L128 544C128 555.5 134.2 566.2 144.2 571.8C154.2 577.4 166.5 577.3 176.4 571.4L320 485.3L463.5 571.4C473.4 577.3 485.7 577.5 495.7 571.8C505.7 566.1 512 555.5 512 544L512 128C512 92.7 483.3 64 448 64L192 64z"/></svg>
       볼 영화
     </RouterLink>
 
     <RouterLink to="/watched" class="tab" active-class="active" exact-active-class="active">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M273 151.1L288 171.8L303 151.1C328 116.5 368.2 96 410.9 96C484.4 96 544 155.6 544 229.1L544 231.7C544 249.3 540.6 267.3 534.5 285.4C512.7 276.8 488.9 272 464 272C358 272 272 358 272 464C272 492.5 278.2 519.6 289.4 544C288.9 544 288.5 544 288 544C272.5 544 257.2 539.4 244.9 529.9C171.9 474.2 32 343.9 32 231.7L32 229.1C32 155.6 91.6 96 165.1 96C207.8 96 248 116.5 273 151.1zM320 464C320 384.5 384.5 320 464 320C543.5 320 608 384.5 608 464C608 543.5 543.5 608 464 608C384.5 608 320 543.5 320 464zM521.4 403.1C514.3 397.9 504.2 399.5 499 406.6L446 479.5L419.2 452.7C413 446.5 402.8 446.5 396.6 452.7C390.4 458.9 390.4 469.1 396.6 475.3L436.6 515.3C439.9 518.6 444.5 520.3 449.2 519.9C453.9 519.5 458.1 517.1 460.9 513.4L524.9 425.4C530.1 418.3 528.5 408.2 521.4 403.1z"/></svg>
       본 영화
     </RouterLink>
 
     <RouterLink to="/stats" class="tab" active-class="active" exact-active-class="active">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM168 280L168 344C168 357.3 178.7 368 192 368C205.3 368 216 357.3 216 344L216 280C216 266.7 205.3 256 192 256C178.7 256 168 266.7 168 280zM248 184L248 344C248 357.3 258.7 368 272 368C285.3 368 296 357.3 296 344L296 184C296 170.7 285.3 160 272 160C258.7 160 248 170.7 248 184zM328 248L328 344C328 357.3 338.7 368 352 368C365.3 368 376 357.3 376 344L376 248C376 234.7 365.3 224 352 224C338.7 224 328 234.7 328 248z"/></svg>
       통계
     </RouterLink>
   </nav>
@@ -33,8 +37,20 @@ export default BottomTabBar;
   height: 64px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
-  background: #fff;
-  border-top: 1px solid #e5e5e5;
+  background-color: #452829;
+}
+
+.tabbar svg{
+  width: 20px;
+  height: 20px;
+  margin-right: 2px;
+}
+.tab.active svg path{
+  fill: #e5e5e5;
+}
+
+.tab svg path{
+  fill: #A18D6D;
 }
 
 .tab{
@@ -42,11 +58,11 @@ export default BottomTabBar;
   align-items: center;
   justify-content: center;
   text-decoration: none;
-  color: #666;
+  color: #A18D6D;
   font-weight: 600;
 }
 
 .tab.active{
-  color: #111;
+  color: #e5e5e5;
 }
 </style>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -8,7 +8,8 @@
       <img class="search-image" src="@/assets/images/header.jpg" alt="search img">
       <div class="search-input-wrap">
         <span class="search-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path
+              d="M480 272C480 317.9 465.1 360.3 440 394.7L566.6 521.4C579.1 533.9 579.1 554.2 566.6 566.7C554.1 579.2 533.8 579.2 521.3 566.7L394.7 440C360.3 465.1 317.9 480 272 480C157.1 480 64 386.9 64 272C64 157.1 157.1 64 272 64C386.9 64 480 157.1 480 272zM272 416C351.5 416 416 351.5 416 272C416 192.5 351.5 128 272 128C192.5 128 128 192.5 128 272C128 351.5 192.5 416 272 416z"/></svg>
         </span>
         <input
             class="search-input"
@@ -21,8 +22,38 @@
     </section>
 
     <section class="search-results">
-      <p v-if="loading" class="search-loading">불러오는 중...</p>
-      <p v-else-if="!movies.length" class="search-empty">결과가 없어요</p>
+      <div v-if="loading" class="search-loading">
+        <p>불러오는 중...</p>
+        <div class="loader loader--style5" title="4">
+          <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+               x="0px" y="0px"
+               width="24px" height="30px" viewBox="0 0 24 30" style="enable-background:new 0 0 50 50;"
+               xml:space="preserve">
+    <rect x="0" y="0" width="4" height="10" fill="#333">
+      <animateTransform attributeType="xml"
+                        attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0" dur="0.6s" repeatCount="indefinite"/>
+    </rect>
+            <rect x="10" y="0" width="4" height="10" fill="#333">
+      <animateTransform attributeType="xml"
+                        attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0.2s" dur="0.6s" repeatCount="indefinite"/>
+    </rect>
+            <rect x="20" y="0" width="4" height="10" fill="#333">
+      <animateTransform attributeType="xml"
+                        attributeName="transform" type="translate"
+                        values="0 0; 0 20; 0 0"
+                        begin="0.4s" dur="0.6s" repeatCount="indefinite"/>
+    </rect>
+  </svg>
+        </div>
+      </div>
+      <div v-else-if="!movies.length" class="search-empty">
+        <span>결과가 없어요</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M528 320C528 205.1 434.9 112 320 112C205.1 112 112 205.1 112 320C112 434.9 205.1 528 320 528C434.9 528 528 434.9 528 320zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM198.1 217.9L224 243.8L249.9 217.9C257.7 210.1 270.4 210.1 278.2 217.9C286 225.7 286 238.4 278.2 246.2L252.3 272.1L278.2 298C286 305.8 286 318.5 278.2 326.3C270.4 334.1 257.7 334.1 249.9 326.3L224 300.4L198.1 326.3C190.3 334.1 177.6 334.1 169.8 326.3C162 318.5 162 305.8 169.8 298L195.7 272.1L169.8 246.2C162 238.4 162 225.7 169.8 217.9C177.6 210.1 190.3 210.1 198.1 217.9zM390.1 217.9L416 243.8L441.9 217.9C449.7 210.1 462.4 210.1 470.2 217.9C478 225.7 478 238.4 470.2 246.2L444.3 272.1L470.2 298C478 305.8 478 318.5 470.2 326.3C462.4 334.1 449.7 334.1 441.9 326.3L416 300.4L390.1 326.3C382.3 334.1 369.6 334.1 361.8 326.3C354 318.5 354 305.8 361.8 298L387.7 272.1L361.8 246.2C354 238.4 354 225.7 361.8 217.9C369.6 210.1 382.3 210.1 390.1 217.9zM320 352C355.3 352 384 380.7 384 416C384 451.3 355.3 480 320 480C284.7 480 256 451.3 256 416C256 380.7 284.7 352 320 352z"/></svg>
+      </div>
       <article v-for="m in movies" :key="m.id" class="movie-card">
         <div class="movie-thumb" aria-hidden="true">
           <img v-if="posterUrl(m.poster_path)" class="movie-thumb-img" :src="posterUrl(m.poster_path)" :alt="m.title"/>
@@ -58,6 +89,7 @@ type TmdbMovie = {
   release_date: string | null;
   vote_average: number;
 };
+
 @Component({name: 'SearchPage'})
 class SearchPage extends Vue {
   query = '';
@@ -150,15 +182,17 @@ class SearchPage extends Vue {
     router.push(`/movie/${m.id}/review`);
   }
 }
+
 export default SearchPage;
 </script>
 <style scoped>
-.search-bar{
+.search-bar {
   position: relative;
   width: 100%;
   height: 280px;
 }
-.search-image{
+
+.search-image {
   position: absolute;
   top: 0;
   left: 0;
@@ -166,7 +200,8 @@ export default SearchPage;
   height: 280px;
   object-fit: cover;
 }
-.search-input-wrap{
+
+.search-input-wrap {
   position: absolute;
   bottom: 20px;
   left: 50%;
@@ -176,11 +211,17 @@ export default SearchPage;
   border-radius: 0;
   background-color: #E8E2D8;
 }
-.search-input-wrap input{
+
+.search-input-wrap input {
   background-color: transparent;
 }
-.search-icon svg{
+
+.search-icon svg {
   width: 16px;
   height: 16px;
 }
+
+
+
+
 </style>
